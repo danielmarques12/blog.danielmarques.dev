73e3de409922a07ddca61c3b0adf0c10292e298c
Daniel Marques <daniel.brz2009@gmail.com>
2023-06-25 17:59:44 -0300

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -39,4 +40,8 @@ export default defineConfig({
       exclude: ["@resvg/resvg-js"],
     },
   },
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
