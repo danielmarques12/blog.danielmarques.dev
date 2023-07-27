9e96a17d94b8cf54a0c4670c35384ad1ccadbefd
Daniel Marques <daniel.brz2009@gmail.com>
2023-07-27 19:13:40 -0300

    feat: remove twitter links

--- src/types.ts
+++ src/types.ts
@@ -19,4 +19,4 @@ export type SocialIcons = {
   [social in SocialMedia]: string;
 };
 
-export type SocialMedia = "Github" | "LinkedIn" | "Mail" | "Twitter";
+export type SocialMedia = "Github" | "LinkedIn" | "Mail";
